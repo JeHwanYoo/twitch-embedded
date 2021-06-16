9aabfcc29c3cbfead38f3e1626c226e63803adeb
JeHwan Yoo <yoojehwan1875@gmail.com>
2021-06-16 23:19:00 +0900

    update

--- README.md
+++ README.md
@@ -1,9 +1,3 @@
-## HOW TO USE
+## TWITCH EMBEDEDDED
 
-```
-https://quirky-wiles-220cc0.netlify.app/
-?button-text=show
-&placeholder=twitch-id
-&twitch-id=twitch
-&parent=quirky-wiles-220cc0.netlify.app
-```
+Hello
---
+++ index.js
@@ -0,0 +1,115 @@
+window.addEventListener('DOMContentLoaded', () => {
+  const anchors = window.location.href.split('#')[1]
+  const twitchIDForm = document.querySelector('#twitch-id')
+  const twitchChatForm = document.querySelector('#twitch-chat-embed')
+  const twitchButton = document.querySelector('#show-twitch')
+  const lTwitchButton = document.querySelector('#live-twtich')
+  const liveList = document.querySelector('#list-live')
+  const parent = window.location.host.split(':')[0]
+  const clinetID = '9bplcpum7fvmne0rol9cubfl4yy8xw'
+  const dTwip = document.querySelector('#d-twip')
+  const dToonation = document.querySelector('#d-toonation')
+
+  var modal = new bootstrap.Modal(document.getElementById('modal-stream'), {
+    keyboard: false,
+  })
+
+  /** Initialize Form */
+  twitchIDForm.value = localStorage.getItem('channel') || 'lck_korea'
+  dTwip.href = `https://twip.kr/donate/${twitchIDForm.value}`
+  dToonation.href = `https://toon.at/donate/${twitchIDForm.value}`
+
+  /** Initailize Twitch Player */
+  const player = new Twitch.Player('twitch-embed', {
+    width: 400,
+    height: 240,
+    channel: twitchIDForm.value,
+    parent: [parent],
+  })
+  twitchChatForm.src = `https://www.twitch.tv/embed/${twitchIDForm.value}/chat?parent=${parent}`
+  dTwip.href = `https://twip.kr/donate/${twitchIDForm.value}`
+
+  /** Change Channel */
+  twitchButton.onclick = () => {
+    player.setChannel(twitchIDForm.value)
+    twitchChatForm.src = `https://www.twitch.tv/embed/${twitchIDForm.value}/chat?parent=${parent}`
+    localStorage.setItem('channel', twitchIDForm.value)
+    dTwip.href = `https://twip.kr/donate/${twitchIDForm.value}`
+    dToonation.href = `https://toon.at/donate/${twitchIDForm.value}`
+  }
+
+  /** Find Live Stream */
+  lTwitchButton.onclick = async () => {
+    if (!anchors) {
+      const a = document.createElement('a')
+      const linkText = document.createTextNode('로그인이 필요합니다.')
+      a.appendChild(linkText)
+      a.href = `https://id.twitch.tv/oauth2/authorize?client_id=${clinetID}&redirect_uri=${window.location.origin}&response_type=token&scope=user_read+user:read:follows`
+      a.classList.add('link-success')
+      liveList.textContent = ''
+      liveList.appendChild(a)
+      return
+    }
+    try {
+      const authData = {}
+      anchors.split('&').forEach(v => {
+        const [key, value] = v.split('=')
+        authData[key] = value
+      })
+      if (!authData['access_token']) {
+        const a = document.createElement('a')
+        const linkText = document.createTextNode('로그인이 필요합니다.')
+        a.appendChild(linkText)
+        a.href = `https://id.twitch.tv/oauth2/authorize?client_id=${clinetID}&redirect_uri=${window.location.origin}&response_type=token&scope=user:read:follows`
+        a.classList.add('link-success')
+        liveList.textContent = ''
+        liveList.appendChild(a)
+      } else {
+        const resUser = await axios({
+          url: `https://api.twitch.tv/kraken/user`,
+          headers: {
+            Accept: 'application/vnd.twitchtv.v5+json',
+            'Client-ID': clinetID,
+            Authorization: `OAuth ${authData['access_token']}`,
+          },
+        })
+        const user = resUser.data
+        const resFollowed = await axios({
+          url: `https://api.twitch.tv/helix/streams/followed?user_id=${user._id}`,
+          headers: {
+            Accept: 'application/vnd.twitchtv.v5+json',
+            'Client-ID': clinetID,
+            Authorization: `Bearer ${authData['access_token']}`,
+          },
+        })
+        const followed = resFollowed.data.data
+        const ul = document.createElement('ul')
+        ul.style = 'max-height: 400px; overflow-y: auto;'
+        ul.classList.add('list-group')
+        followed.forEach(v => {
+          const button = document.createElement('button')
+          const textNode = document.createTextNode(`${v.user_name}`)
+          button.appendChild(textNode)
+          button.classList.add('list-group-item')
+          button.classList.add('list-group-item-action')
+          button.onclick = () => {
+            twitchIDForm.value = v.user_login
+            player.setChannel(v.user_login)
+            twitchChatForm.src = `https://www.twitch.tv/embed/${v.user_login}/chat?parent=${parent}`
+            localStorage.setItem('channel', v.user_login)
+            dTwip.href = `https://twip.kr/donate/${v.user_login}`
+            dToonation.href = `https://toon.at/donate/${v.user_login}`
+            modal.hide()
+          }
+          ul.appendChild(button)
+        })
+
+        liveList.textContent = ''
+        liveList.appendChild(ul)
+      }
+    } catch (error) {
+      console.log(error)
+      liveList.innerText = '에러가 발생했습니다.'
+    }
+  }
+})
